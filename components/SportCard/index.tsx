c31c8eef89e6692ee0c63e363bbf5e02c25a7cc4
Shubbham Jain <shubham.jain368@gmail.com>
2022-12-07 20:26:10 +0530

    fix: sport select card

--- components/SportCard/index.tsx
+++ components/SportCard/index.tsx
@@ -1,46 +1,84 @@
 // libraries
+import Image from 'next/image'
+import { OverridableComponent } from '@mui/material/OverridableComponent'
 import {
+  Box,
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
+  Stack,
+  SvgIconTypeMap,
   Typography,
 } from '@mui/material'
-import cricket from 'assets/cricket.png'
-import Image from 'next/image'
-
+import { SportsFootball, SportsSoccer } from '@mui/icons-material'
 // contexts and hooks
 
 // styles
 
 // assets
 
-interface Props {}
+interface Props {
+  sportName: string
+  sportImg: StaticImageData
+  sportIcon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
+    muiName: string
+  }
+}
 
 const SportCard = (props: Props) => {
+  const { sportIcon: SportIcon, sportImg, sportName } = props
   return (
     <Card
       sx={{
         maxWidth: 200,
+        borderRadius: '4px',
+        position: 'relative',
+        background: '#0EB634',
         ':hover': { cursor: 'pointer' },
       }}
     >
-      <CardMedia sx={{ display: 'inline' }}>
+      <CardMedia>
         <Image
+          src={sportImg}
+          width={'220px'}
+          height={'280px'}
           objectFit="fill"
-          src={cricket}
-          alt="cc"
-          width={'200px'}
-          height={'250px'}
+          alt="sport image"
         />
       </CardMedia>
-      {/* <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+
+      <Box
+        position={'absolute'}
+        height={'260px'}
+        width={'140px'}
+        top={100}
+        left={5}
+        sx={{
+          backdropFilter: 'blur(2px)',
+          transform: 'matrix(-0.5, 0.87, 0.87, 0.5, 0, 0)',
+          background:
+            'linear-gradient(186.78deg, #07070D 17.47%, rgba(7, 7, 13, 0) 125.64%)',
+        }}
+      ></Box>
+      <Stack position={'absolute'} top={150} left={20}>
+        <SportIcon sx={{ width: '40px', height: '40px', color: '#fff' }} />
+
+        <Typography
+          mt={1}
+          mb={'2px'}
+          fontWeight={700}
+          fontSize={'16px'}
+          color={'#0EB634'}
+        >
+          {sportName}
         </Typography>
-      </CardContent> */}
+
+        <Typography fontWeight={400} fontSize={'14px'} color={'#fff'}>
+          Bettings Open
+        </Typography>
+      </Stack>
     </Card>
   )
 }
