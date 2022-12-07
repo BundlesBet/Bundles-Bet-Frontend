08ee86b8193f97e07cbe88cba54321c000dd900c
Shubbham Jain <shubham.jain368@gmail.com>
2022-12-07 20:27:38 +0530

    files: added assets

--- assets/index.tsx
+++ assets/index.tsx
@@ -2,12 +2,23 @@
  * @description the root export file for getting the all assets used in the project
  */
 import key from './key.png'
+import NFL from './NFL.png'
 import Link from './link.png'
 import Logo from './Logo.png'
 import wallet from './wallet.png'
 import Discord from './discord.png'
+import Football from './Football.png'
 import ProfilePic from './ProfilePic.png'
 import background from './background.png'
-import cricket from './cricket.png'
 
-export { background, cricket, Discord, key, Logo, Link, ProfilePic, wallet }
+export {
+  background,
+  Discord,
+  Football,
+  key,
+  Logo,
+  Link,
+  NFL,
+  ProfilePic,
+  wallet,
+}
