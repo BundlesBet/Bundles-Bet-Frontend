4a9c2c9bbcdb935d0800f345d6718f57bfba4092
Shubbham Jain <shubham.jain368@gmail.com>
2023-03-10 16:03:15 +0530

    fix(utils): fix: env variable
    fixed the NODE_ENV variable use

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -7,7 +7,7 @@ import { ignoreRoutes } from "./apiCalls";
 
 const axios = Axios.create({
   baseURL:
-    process.env.NODE_ENV === "production"
+    process.env.NEXT_PUBLIC_NODE_ENV === "production"
       ? `${process.env.NEXT_PUBLIC_PROD_BASE_URL}/${process.env.NEXT_PUBLIC_VERSION}`
       : `${process.env.NEXT_PUBLIC_BASE_URL}/${process.env.NEXT_PUBLIC_VERSION}`,
 });
